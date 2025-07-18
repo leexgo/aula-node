db4c6a17958764d636c20b804607637cbddc6279
AlexGo2 <alex.gomes.dev@gmail.com>
2025-07-18 16:04:54 -0300

    CRUD, View engine (ejs)

--- modules/express.js
+++ modules/express.js
@@ -5,6 +5,16 @@ const app = express();
 const port = 8080;
 
 app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", "src/views");
+
+app.use((req, res, next) => {
+  console.log("Request type:", req.method);
+  console.log("Content type:", req.headers["content-type"]);
+  console.log("Date:", new Date());
+
+  next();
+});
 
 app.get("/users", async (req, res) => {
   try {
@@ -13,6 +23,11 @@ app.get("/users", async (req, res) => {
   } catch (error) {
     return res.status(500).send(error.message);
   }
+});
+
+app.get("/views/users", async (req, res) => {
+  const users = await UserModel.find({});
+  res.render("index", { users });
 });
 
 app.get("/users/:id", async (req, res) => {
@@ -39,6 +54,20 @@ app.post("/users", async (req, res) => {
   try {
     const user = await UserModel.create(req.body);
     res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndDelete(id);
+
+    if (!user) {
+      res.status(404).send("Usuário não encontrado");
+    }
+    return res.status(200).send("Usuário deletado");
   } catch (error) {
     res.status(500).send(error.message);
   }
